71670be551d96c06a723cb4622dab63f37785650
Arian <arian.kulmer@web.de>
2022-10-11 22:18:21 +0200

    Camera: Skip stream size check for whitelisted apps
    This is a squash of the following CLO commits:
    
    commit 84b2ea267f2b5a5b8925758ab8f9e9f0db4f6d53
    Author: Pavan Kumar Mc <quic_pmc@quicinc.com>
    Date:   Fri Nov 12 21:21:46 2021 +0530
    
        Camera: Avoid roundBufferDimensionsNearest for AIDE2 YUV streams
    
        - AIDE2 is vendor enhanced feature which needs special resolutions
          for full and downscaled YUV streams.Those resolutions are not
          populated in static capabilities. So avoid roundBufferDimensionsNearest
          for YUV streams in privileged client case.
    
        CRs-Fixed: 3012886
    
        Change-Id: Ia1d134edd1fac2ffd454137aa2a4aa5b9d40c7a7
    
    commit 35df5effc321d630e711161fba02614f0c61abdd
    Author: Pavan Kumar Mc <pmc@codeaurora.org>
    Date:   Tue Aug 17 10:28:21 2021 +0530
    
        Camera: Support raw stream creation for priviledged APPs
    
        - Raw stream creation failing as priviledged APP check set to FALSE
          for new set of call flow to roundBufferDimensionNearest. Setting
          the condition to TRUE.
    
        CRs-Fixed: 3011444
    
        Change-Id: Ia3d5e4744a78b06c00e99fb5be856b24acbff599
    
    commit 573cb73a1c6c4a8bf13791b98c5d539ebffa2d0f
    Author: Pavan Kumar Mc <pmc@codeaurora.org>
    Date:   Thu Jun 24 11:19:35 2021 +0530
    
        Camera: Master callback mode support for MCX raw
    
        - Raw stream creation failing for size greater than logical
          camera resolution as size cannot exceed max raw stream size
          in available stream configuration.
    
        - Allow creating raw streams of greater size for logical camera
    
        CRs-Fixed: 2968932
    
        Change-Id: Ibacc3c72e34b2f137546288f67bcdb4e0a38aa23
    
    commit 15de50a2fe471e7d3cc498286f0de31282303fb0
    Author: Pavan Kumar Mc <pmc@codeaurora.org>
    Date:   Wed Apr 21 17:59:46 2021 +0530
    
        Camera: Master callback mode support for MCX raw
    
        - Raw stream creation failing for size greater than logical
          camera resolution as size cannot exceed max raw stream size
          in available stream configuration.
    
        - Allow creating raw streams of greater size for logical camera
    
        CRs-Fixed: 2860300
    
        Change-Id: Ic0f99e55b1519596e49ff2af14895cea1a19c752
    
    commit cee145eaaf32faf0b99f46f957a9fb6ad66abccf
    Author: Gaoxiang Chen <gaochen@codeaurora.org>
    Date:   Wed Jun 21 12:57:08 2017 +0530
    
        Camera: Skip stream size check for whitelisted apps.
    
        Issue:
        For quadracfa capture, Blob/YUV output streams need to be
        configured with custom dimensions which will not be available
        in advertised stream configurations map.
    
        Fix:
        Skip the stream size check for whitelisted apps to allow
        configuration of streams with custom dimensions.
    
        Setprop to be used:
        adb shell setprop persist.vendor.camera.privapp.list <pack1,pack2>
    
        Change-Id: Id94b40c94f42bf4579dc6d8bb6273003312ea669
    
    Change-Id: If12f9696fbc78edbd5fb4345a48818fa5d74752f

--- services/camera/libcameraservice/api2/CameraDeviceClient.cpp
+++ services/camera/libcameraservice/api2/CameraDeviceClient.cpp
@@ -23,6 +23,8 @@
 #endif
 //#define LOG_NDEBUG 0
 
+#include <android-base/properties.h>
+#include <android-base/strings.h>
 #include <camera/CameraUtils.h>
 #include <camera/StringUtils.h>
 #include <camera/camera2/CaptureRequest.h>
@@ -95,9 +97,17 @@ CameraDeviceClient::CameraDeviceClient(
       mInputStream(),
       mStreamingRequestId(REQUEST_ID_NONE),
       mRequestIdCounter(0),
+      mPrivilegedClient(false),
       mOverrideForPerfClass(overrideForPerfClass),
       mOriginalCameraId(originalCameraId),
       mIsVendorClient(isVendorClient) {
+
+    std::vector<std::string> privilegedClientList = android::base::Split(
+            android::base::GetProperty("persist.vendor.camera.privapp.list", ""), ",");
+    auto it = std::find(privilegedClientList.begin(), privilegedClientList.end(),
+            clientPackageName);
+    mPrivilegedClient = it != privilegedClientList.end();
+
     ATRACE_CALL();
     ALOGI("CameraDeviceClient %s: Opened", cameraId.c_str());
 }
@@ -195,6 +205,7 @@ status_t CameraDeviceClient::initializeImpl(TProviderPtr providerPtr,
             strerror(-res), res);
         return res;
     }
+    mDevice->setPrivilegedClient(mPrivilegedClient);
     return OK;
 }
 
@@ -1026,7 +1037,8 @@ binder::Status CameraDeviceClient::createStream(
 #endif
                 , mCameraIdStr,
                 mDevice->infoPhysical(physicalCameraId), sensorPixelModesUsed, dynamicRangeProfile,
-                streamUseCase, timestampBase, mirrorMode, colorSpace, /*respectSurfaceSize*/false);
+                streamUseCase, timestampBase, mirrorMode, colorSpace, /*respectSurfaceSize*/false,
+                mPrivilegedClient);
 
         if (!res.isOk())
             return res;
@@ -1421,7 +1433,7 @@ binder::Status CameraDeviceClient::updateOutputConfiguration(int streamId,
                 ,
                 mCameraIdStr, mDevice->infoPhysical(physicalCameraId), sensorPixelModesUsed,
                 dynamicRangeProfile, streamUseCase, timestampBase, mirrorMode, colorSpace,
-                /*respectSurfaceSize*/ false);
+                /*respectSurfaceSize*/ false, mPrivilegedClient);
         if (!res.isOk()) return res;
 
         streamInfos.push_back(outInfo);
@@ -1823,7 +1835,7 @@ binder::Status CameraDeviceClient::finalizeOutputConfigurations(int32_t streamId
 #endif
                 , mCameraIdStr, mDevice->infoPhysical(physicalId),
                 sensorPixelModesUsed, dynamicRangeProfile, streamUseCase, timestampBase, mirrorMode,
-                colorSpace, /*respectSurfaceSize*/ false);
+                colorSpace, /*respectSurfaceSize*/ false, mPrivilegedClient);
 
         if (!res.isOk()) return res;
 
